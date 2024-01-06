a47e542702534c3731a290e183b0e0a7e0985b55
Ilija Leko <ilija.leko.86@gmail.com>
2024-01-06 17:16:50 +0100

    Add global validation pipe

--- apps/api/smb-backend/src/main.ts
+++ apps/api/smb-backend/src/main.ts
@@ -1,15 +1,22 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // Automatically transform payloads to DTO instances
+      transform: true,
+      // Disable error messages in production
+      disableErrorMessages:
+        process.env.NODE_ENV === 'production' ? true : false,
+      // Forbid unknown values instead of stripping them away
+      forbidNonWhitelisted: true,
+      whitelist: true,
+    })
+  );
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
